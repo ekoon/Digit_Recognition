2933158993205bf01fff6a19ee37b13c91a481b5
unknown <aerospike1111@gmail.com>
2014-05-01 18:50:23 -0400

    add a class called who to have speakers index

---
+++ src/vowel_detection/Who.java
@@ -0,0 +1,41 @@
+package vowel_detection;
+
+public class Who {
+	
+    private static int MALE   = 0;
+    private static int FEMALE = 1; 
+
+    Speaker[] speakers = {
+        new Speaker ("Andrew" , 0 , MALE  ),
+        new Speaker ("Bill"	  , 1 , MALE  ),
+        new Speaker ("David"  , 2 , MALE  ),
+        new Speaker ("Mark"   , 3 , MALE  ),
+        new Speaker ("Jo"     , 4 , FEMALE),
+        new Speaker ("Kate"   , 5 , FEMALE),
+        new Speaker ("Penny"  , 6 , FEMALE),
+        new Speaker ("Rose"   , 7 , FEMALE),
+        new Speaker ("Mike"   , 8 , MALE  ),
+        new Speaker ("Nick"   , 9 , MALE  ),
+        new Speaker ("Rich"   , 10, MALE  ),
+        new Speaker ("Tim"    , 11, MALE  ),
+        new Speaker ("Sarah"  , 12, FEMALE),
+        new Speaker ("Sue"    , 13, FEMALE),
+        new Speaker ("Wendy"  , 14, FEMALE)
+    };
+    
+	public Speaker[] AllSpeakers(){					
+		return speakers;
+	}
+    
+	public Speaker IndexSpeakers(int index){					
+		return speakers[index];
+	}
+	
+	public String SpeakersName(int index){					
+		return speakers[index].first_name();
+	}
+	
+	public String SpeakersSex(int index){					
+		return speakers[index].sexString();
+	}
+}
